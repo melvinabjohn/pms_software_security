3bc9d5b8736fe21b7f0b6984343cba6293f9e0bd
Melvin Abraham <melvinabjohn@gmail.com>
2023-01-11 14:20:15 +0100

    included gitignore and changed service names

--- Pms/Test1/Controllers/PMSController.cs
+++ Pms/Test1/Controllers/PMSController.cs
@@ -12,10 +12,7 @@ namespace Test1.Controllers;
 [Route("[controller]")]
 public class PMSController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+
     readonly IPMSService _pmsService;
 
     private readonly ILogger<PMSController> _logger;
@@ -26,17 +23,6 @@ public class PMSController : ControllerBase
         _pmsService = pMSService;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Test()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
 
     /// <summary>
     /// Get the current policy Details
